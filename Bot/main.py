513a7043ab3ad3646137c73e9fe6fc1c6e6ab346
Foxilyan <nikmor1602@gmail.com>
2024-10-09 16:45:13 +0300

    Fixed registration

--- Bot/main.py
+++ Bot/main.py
@@ -62,26 +62,29 @@ def roll(message):
 
 @bot.message_handler(commands=['register'])
 def start_register(message):
-    if len(message.text.split(' ')) == 1:
-        bot.send_message(message.chat.id, 'Введите команду вместе с именем персонажа')
-        return
-    name = message.text.replace('/register ', '')
-    character_sheet = deepcopy(empty_sheet)
-    for stats in character_sheet.keys():
-        for stat in character_sheet[stats]:
+    sheet = deepcopy(empty_sheet)
+    bot.send_message(message.chat.id, 'Введите имя персонажа')
+    bot.register_next_step_handler(message, set_name, sheet)
+
+
+def set_name(message, sheet):
+    name = message.text
+    for stats in sheet:
+        for stat in sheet[stats]:
             bot.send_message(message.chat.id, f'Введите значение {stat}')
-            bot.register_next_step_handler(message, register_stat, stat, stats, character_sheet)
-            while character_sheet[stats][stat] is None:
+            bot.register_next_step_handler(message, set_stat, stat, stats, sheet)
+            while sheet[stats][stat] is None:
                 pass
-
-    with open(f'../Characters/{name}.json', 'w', encoding='utf-8') as file:
-        json.dump(character_sheet, file, indent=4)
-        bot.send_message(message.chat.id, f'Персонаж {name} сохранен')
+    bot.send_message(message.chat.id, str(sheet))
+    bot.send_message(message.chat.id, str(empty_sheet))
 
 
-
-def register_stat(message, stat, stats, character_sheet):
-    character_sheet[stats][stat] = int(message.text)
+def set_stat(message, stat, stats, sheet):
+    try:
+        sheet[stats][stat] = int(message.text)
+    except BaseException:
+        bot.send_message(message.chat.id, 'Дебил, давай нормально')
+        bot.register_next_step_handler(message, set_stat, stat, stats, sheet)
 
 
 @bot.message_handler(commands=['show'])
